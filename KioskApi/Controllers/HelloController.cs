ca0b1f50c9ab0fe8e3e2729a7387821cfe5d2783
woojun0037 <67630480+woojun0037@users.noreply.github.com>
2026-04-09 22:49:50 +0900

    Project Setting

---
+++ KioskApi/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KioskApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API 연결 성공");
+        }
+    }
+}
