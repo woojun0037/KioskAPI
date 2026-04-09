8227c875e9523053350c3340359d3012d8c1879f
woojun0037 <67630480+woojun0037@users.noreply.github.com>
2026-04-09 23:32:27 +0900

    상품조회 API 추가

---
+++ KioskApi/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KioskApi.Models;
+
+namespace KioskApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "아메리카노", Price = 1800},
+                new Product { Id = 2, Name = "카페라떼"  , Price = 2500},
+                new Product { Id = 3, Name = "카푸치노"  , Price = 3000}
+            };
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "아메리카노", Price = 1800},
+                new Product { Id = 2, Name = "카페라떼"  , Price = 2500},
+                new Product { Id = 3, Name = "카푸치노"  , Price = 3000}
+            };
+
+            var product = products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+    }
+}
---
+++ KioskApi/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace KioskApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int Price { get; set; }
+    }
+}
